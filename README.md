1012d2cf5cfa958af73d293c80a3b737ebbe19b3
Tim Birkett <tim@servervet.co>
2015-09-11 00:36:52 +0100

    Reworked to allow arguments and updated README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # F5-BIG-IP-Blacklist
+This is a quick script I threw together to update a couple of F5 BIG-IP 1600's I used to play with (manage and maintain).
+
+Initially there was no form of blacklisting applied and numerous bots, spammers etc were POSTing and GETing all sorts of stuff to **ALL THE THINGS**.
+
+I used the awesome blocklist from the guys over at: [blocklist.de](http://www.blocklist.de/en/index.html) - THANKS GUYS!
+
+Feel free to update this. For example, you could easily add the AWS EC2 IP ranges to prevent cloud based scrapers / DDoS attempts. In fact AWS provide a lovely JSON document at: https://ip-ranges.amazonaws.com/ip-ranges.json which could easily be parsed into the data group.
+
+```
+usage: update_blocklist.py [-h] -a HOST -u USERNAME -p PASSWORD -g DEVICEGROUP
+                           [-d PARTITION]
+
+Update blocklist.de data group on F5 BIG-IP device.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a HOST, --host HOST  ip address of an F5 BIG-IP device
+  -u USERNAME, --username USERNAME
+                        user to authenticate with
+  -p PASSWORD, --password PASSWORD
+                        password to authenticate with
+  -g DEVICEGROUP, --devicegroup DEVICEGROUP
+                        device group of the HA pair
+  -d PARTITION, --partition PARTITION
+                        partition to add the data group to
+```
--- update_blocklist.py
+++ update_blocklist.py
@@ -1,60 +1,59 @@
-#!env/bin/python
+#/usr/bin/env python
 
-import socket
-import urllib
-import urllib2
-import bigsuds
+import sys, socket, urllib, urllib2, argparse, bigsuds
 
-# Set some variables.
+def main():
 
-f5ip = '<<IP of F5 device>>'
-f5user = '<<Username to access device>>'
-f5pass = '<<password to F5 device>>'
-device_group = '<< F5 device group>>'
-partition = '<<Partition>>'
+    parser = argparse.ArgumentParser(description='Update blocklist.de data group on F5 BIG-IP device.')
+    parser.add_argument('-a', '--host', type=str, help='ip address of an F5 BIG-IP device', required=True)
+    parser.add_argument('-u', '--username', type=str, help='user to authenticate with', required=True)
+    parser.add_argument('-p', '--password', type=str, help='password to authenticate with', required=True)
+    parser.add_argument('-g', '--devicegroup', type=str, help='device group of the HA pair', required=True)
+    parser.add_argument('-d', '--partition', type=str, help='partition to add the data group to', default='Common')
+    args = parser.parse_args()
 
-# Leave this unless you also change the iRule
-data_group = "ext_blocklist"
+    # Leave this unless you also change the iRule
+    data_group = "ext_blocklist"
 
-address_list = [{'name':'/' partition + '/' + data_group, 'members':[]}]
+    # Create an empty data group to add IPs to block to
+    address_list = [{'name':'/' + args.partition + '/' + data_group, 'members':[]}]
 
-# Get IPs from the blocklist
-iplisturl = "http://lists.blocklist.de/lists/all.txt"
-data = {'ipv6':'0', 'export_type':'text', 'submit':'Export'}
-data = urllib.urlencode(data)
-req = urllib2.Request(iplisturl, data)
-response = urllib2.urlopen(req)
+    # Get IPs from the blocklist
+    iplisturl = "http://lists.blocklist.de/lists/all.txt"
+    data = urllib.urlencode({'ipv6':'0', 'export_type':'text', 'submit':'Export'})
+    req = urllib2.Request(iplisturl, data)
+    response = urllib2.urlopen(req)
 
-list = response.read()
-blocklist = set(list.splitlines())
+    blocklist = set(response.read().splitlines())
 
-for ip in blocklist:
-  # Hacky way to test if it's an IP
-  try:
-    socket.inet_aton(ip)
-    address_list[0]['members'].append({'netmask':'255.255.255.255', 'address':ip})    
-  except socket.error:
-    pass
+    for ip in blocklist:
+        # Hacky way to test if it's an IP
+        try:
+            socket.inet_aton(ip)
+            address_list[0]['members'].append({'netmask':'255.255.255.255', 'address':ip})
+        except socket.error:
+            pass
 
-# Make sure that the list contains some stuff
-if len(address_list[0]['members']) > 0:
-  b = bigsuds.BIGIP(hostname = f5ip, username = f5user, password = f5pass)
-  bigtrans = b.with_session_id()
-  bigtrans.System.Session.set_transaction_timeout(99)
-  try:
-    with bigsuds.Transaction(bigtrans):
-      bigtrans.Management.Partition.set_active_partition(partition)
-      try:
-        bigtrans.LocalLB.Class.modify_address_class(address_list)
-      except bigsuds.OperationFailed:
-        try: 
-          bigtrans.LocalLB.Class.add_address_class(address_list)
-        except bigsuds.OperationFailed:
-          try:
-            bigtrans.LocalLB.Class.delete_class(data_group)
-            bigtrans.LocalLB.Class.add_address_class(address_list)
-          except bigsuds.OperationFailed, e: 
-            print e   
-    bigtrans.System.ConfigSync.synchronize_to_group(device_group)
-  except bigsuds.OperationFailed, e:
-    print e
+    # Make sure that the list contains some stuff
+    if len(address_list[0]['members']) > 0:
+        print len(address_list[0]['members'])
+        b = bigsuds.BIGIP(hostname = args.host, username = args.username, password = args.password)
+        bigtrans = b.with_session_id()
+        bigtrans.System.Session.set_transaction_timeout(10)
+
+        try:
+            with bigsuds.Transaction(bigtrans):
+                bigtrans.Management.Partition.set_active_partition(args.partition)
+                bigtrans.LocalLB.Class.delete_class(data_group)
+                bigtrans.LocalLB.Class.add_address_class(address_list)
+
+                # Make sure the updated config is synced between devices
+                bigtrans.System.ConfigSync.synchronize_to_group(args.devicegroup)
+                return 0
+
+        except bigsuds.OperationFailed, e:
+            print e
+            return 1
+
+if __name__ == "__main__":
+    sys.exit(main())
